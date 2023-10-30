b4b82cb12ebfce53ba6de08c2044f6f30e3d3d0e
Ade Putra <reyadek@gmail.com>
2023-10-30 14:09:41 +0700

    first commit

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,109 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+const db = new PrismaClient();
+
+interface IUser {
+  id?: number;
+  email: string;
+  password: string;
+  createdAt: string;
+  UpdatedAt: string;
+}
+
+export const UserController = {
+  getUsers: async () => {
+    return await db.user.findMany();
+  },
+
+  getUserById: async (id: number) => {
+    const user = await db.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (user) {
+      return user;
+    } else {
+      return {
+        error: "user not found",
+      };
+    }
+  },
+
+  createUser: async (body: IUser) => {
+    try {
+      return await db.user.create({
+        data: body,
+      });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === "P2002") {
+          console.log(
+            "There is a unique constraint violation, a new user cannot be created with this email"
+          );
+          return {
+            error: "Username must be unique",
+          };
+        }
+      }
+      throw e;
+    }
+  },
+
+  updateUser: async (id: number, body: IUser) => {
+    const find_user = await db.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    try {
+      if (find_user) {
+        return await db.user.update({
+          where: {
+            id: id,
+          },
+          data: {
+            email: body.email,
+            password: body.password,
+          },
+        });
+      } else {
+        return {
+          error: "user not found",
+        };
+      }
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === "P2002") {
+          console.log(
+            "There is a unique constraint violation, a new user cannot be created with this email"
+          );
+          return {
+            error: "Username must be unique",
+          };
+        }
+      }
+      throw e;
+    }
+  },
+
+  deleteUser: async (id: number) => {
+    const find_user = await db.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (find_user) {
+      return await db.user.delete({
+        where: {
+          id: id,
+        },
+      });
+    } else {
+      return {
+        error: "user not found",
+      };
+    }
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,49 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { UserController } from "./controllers/UserController";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+
+const userIdDTO = t.Object({
+  id: t.Numeric(),
+});
+
+const userDTO = t.Object({
+  id: t.Numeric(),
+  email: t.String(),
+  password: t.String(),
+  createdAt: t.String(),
+  UpdatedAt: t.String(),
+});
+
+app.get("/", () => "Hello Elysia");
+
+app.group("/user", (app) =>
+  app
+    .get("/", UserController.getUsers)
+    .get("/:id", ({ params: { id } }) => UserController.getUserById(id), {
+      params: userIdDTO,
+    })
+    .post("/create", ({ body }) => UserController.createUser(body), {
+      body: userDTO,
+    })
+    .post(
+      "/update/:id",
+      ({ params: { id }, body }) => UserController.updateUser(id, body),
+      {
+        params: userIdDTO,
+        body: userDTO,
+      }
+    )
+    .post(
+      "/delete/:id",
+      ({ params: { id } }) => UserController.deleteUser(id),
+      {
+        params: userIdDTO,
+      }
+    )
+);
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
